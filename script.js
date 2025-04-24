20f1f2deccf917c9feb84cb02ef4b154e3936723
anveqn <205069225+anveqn@users.noreply.github.com>
2025-04-24 21:18:26 +0200

    Add a function to play a single round

--- script.js
+++ script.js
@@ -3,21 +3,65 @@ let getComputerChoice = () => {
     let finalChoice;
     switch (randomChoice) {
         case 0:
-            finalChoice = "Rock";
+            finalChoice = "rock";
             break;
         case 1:
-            finalChoice = "Paper";
+            finalChoice = "paper";
             break;
         case 2:
-            finalChoice = "Scissors";
+            finalChoice = "scissors";
             break;         
     }
+    console.log(`computer's choice is ${finalChoice}`);
     return finalChoice;
 };
 
 let getHumanChoice = () => {
     let human = prompt("Make your choice.").toLowerCase();
-}
+    return human;
+};
 
 let humanScore = 0;
 let computerScore = 0;
+
+let playRound = (humanChoice, computerChoice) => {
+    switch (humanChoice){
+        case "rock":
+            if (computerChoice==="scissors"){
+                console.log("You win! Rock beats Scissors");
+                humanScore++;
+            } else if (computerChoice==="paper"){
+                console.log("You lose, Paper beats Rock");
+                computerScore++;
+            }else {
+                console.log("It's a draw! No points gained.")
+            };
+            break;
+        case "paper":
+            if (computerChoice==="rock"){
+                console.log("You win! Paper beats Rock");
+                humanScore++;
+            } else if (computerChoice==="scissors"){
+                console.log("You lose, Scissors beat Paper");
+                computerScore++;
+            }else {
+                console.log("It's a draw! No points gained.")
+            };
+            break;
+        case "scissors":
+            if (computerChoice==="paper"){
+                console.log("You win! Scissors beat Paper");
+                humanScore++;
+            } else if (computerChoice==="rock"){
+                console.log("You lose, Rock beats Scissors");
+                computerScore++;
+            }else {
+                console.log("It's a draw! No points gained.")
+            };
+            break;
+    }
+};
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
