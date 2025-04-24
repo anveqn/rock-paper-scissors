85f333c2336429d4d63de1f27fe488ce8697b2d9
anveqn <205069225+anveqn@users.noreply.github.com>
2025-04-24 22:20:16 +0200

    Add a five round game functionality

--- script.js
+++ script.js
@@ -12,56 +12,74 @@ let getComputerChoice = () => {
             finalChoice = "scissors";
             break;         
     }
-    console.log(`computer's choice is ${finalChoice}`);
     return finalChoice;
 };
 
 let getHumanChoice = () => {
-    let human = prompt("Make your choice.").toLowerCase();
+    let human = prompt("Make your choice!").toLowerCase();
     return human;
 };
 
-let humanScore = 0;
-let computerScore = 0;
+const playGame = () => {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    let playRound = (humanChoice, computerChoice) => {
+        switch (humanChoice){
+            case "rock":
+                if (computerChoice==="scissors"){
+                    console.log("You win! Rock beats Scissors");
+                    humanScore++;
+                } else if (computerChoice==="paper"){
+                    console.log("You lose, Paper beats Rock");
+                    computerScore++;
+                }else {
+                    console.log("It's a draw! No points gained.")
+                };
+                break;
+            case "paper":
+                if (computerChoice==="rock"){
+                    console.log("You win! Paper beats Rock");
+                    humanScore++;
+                } else if (computerChoice==="scissors"){
+                    console.log("You lose, Scissors beat Paper");
+                    computerScore++;
+                }else {
+                    console.log("It's a draw! No points gained.")
+                };
+                break;
+            case "scissors":
+                if (computerChoice==="paper"){
+                    console.log("You win! Scissors beat Paper");
+                    humanScore++;
+                } else if (computerChoice==="rock"){
+                    console.log("You lose, Rock beats Scissors");
+                    computerScore++;
+                }else {
+                    console.log("It's a draw! No points gained.")
+                };
+                break;
+        }
+    };
 
-let playRound = (humanChoice, computerChoice) => {
-    switch (humanChoice){
-        case "rock":
-            if (computerChoice==="scissors"){
-                console.log("You win! Rock beats Scissors");
-                humanScore++;
-            } else if (computerChoice==="paper"){
-                console.log("You lose, Paper beats Rock");
-                computerScore++;
-            }else {
-                console.log("It's a draw! No points gained.")
-            };
-            break;
-        case "paper":
-            if (computerChoice==="rock"){
-                console.log("You win! Paper beats Rock");
-                humanScore++;
-            } else if (computerChoice==="scissors"){
-                console.log("You lose, Scissors beat Paper");
-                computerScore++;
-            }else {
-                console.log("It's a draw! No points gained.")
-            };
-            break;
-        case "scissors":
-            if (computerChoice==="paper"){
-                console.log("You win! Scissors beat Paper");
-                humanScore++;
-            } else if (computerChoice==="rock"){
-                console.log("You lose, Rock beats Scissors");
-                computerScore++;
-            }else {
-                console.log("It's a draw! No points gained.")
-            };
-            break;
+    while (humanScore<5 && computerScore<5){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        console.log(`Your choice is ${humanSelection}. Computer's choice is ${computerSelection}`);
+        playRound(humanSelection, computerSelection);
+        console.log(`Current score: You ${humanScore} - ${computerScore} CPU`);
+        console.log(" ");
+    }
+
+    if(humanScore == 5){
+        console.log(`Game is over! You won! Final Score: You ${humanScore} - ${computerScore} CPU`);
+        humanScore = 0;
+        computerScore = 0;
+    } else if (computerScore == 5){
+        console.log(`Game is over! You lost... Final Score: You ${humanScore} - ${computerScore} CPU`);
+        humanScore = 0;
+        computerScore = 0;
     }
 };
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+playGame();
